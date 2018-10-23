247e3891dbf058ad54f16565f1aa7db8d36a5bbc
przk <43840987+przk@users.noreply.github.com>
2018-10-23 17:19:18 +0200

    zadanie wykonane

--- src/neville.cpp
+++ src/neville.cpp
@@ -1,2 +1,22 @@
-#include "neville.h"
+#include "..\include\neville.h"
 
+double neville(double n, double *x, double *f, int sz)
+{
+	double **T=new double*[sz];
+	for(int i=0; i<sz; i++)
+	{
+		T[i]=new double[sz];
+		for(int j=0; j<sz; j++)
+			T[i][j]=0;
+	}
+	
+	for(int i=0; i<sz; i++)
+	{
+		T[i][0]=f[i];
+		for(int k=1; k<=i; k++)
+		{
+			T[i][k]=T[i][k-1]+( T[i][k-1]-T[i-1][k-1] )/( (n-x[i-k])/(n-x[i])-1 );
+		}
+	}
+	return T[sz-1][sz-1];
+}
